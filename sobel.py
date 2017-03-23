27d0d1a5427ac126e2863813b23383b178602bc1
dabl8647 <dabl8647@colorado.edu>
2017-03-23 00:07:20 -0600

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
-# NOTE: MUST install PIL 
+# NOTE: MUST install PIL
 # http://www.pythonware.com/products/pil/
 
-import numpy
-import scipy
-from scipy import ndimage
 import argparse
 
 # https://docs.python.org/3/library/argparse.html
@@ -13,23 +10,60 @@ parser = argparse.ArgumentParser()
 parser.add_argument("imagename", help="provide file name of image want to run sobel operator")
 args = parser.parse_args()
 
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.imread.html#scipy.misc.imread
-# read in image using defaults
-im = scipy.misc.imread(args.imagename)
+import numpy
+import scipy
+from scipy import ndimage, misc
 
-# https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.astype.html#numpy.ndarray.astype
-# https://docs.scipy.org/doc/numpy/user/basics.types.html
-# cast image array to cast to int32 type
-im = im.astype('int32')
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.imread.html#scipy.misc.imread
+# read in image as grayscale
+im = scipy.misc.imread(args.imagename, flatten=True)
+
+# just some basic statistics about the image
+print im.dtype, im.shape
+
+# see http://www.scipy-lectures.org/advanced/image_processing/auto_examples/plot_find_edges.html
 
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.sobel.html
 # apply sobel
-dx = ndimage.sobel(im, 0)  # horizontal derivative
-dy = ndimage.sobel(im, 1)  # vertical derivative
+dx = ndimage.sobel(im, axis=0, mode='constant')  # horizontal derivative
+dy = ndimage.sobel(im, axis=1, mode='constant')  # vertical derivative
 
 # https://docs.scipy.org/doc/numpy/reference/generated/numpy.hypot.html
 mag = numpy.hypot(dx, dy)  # magnitude
 
-mag *= 255.0 / numpy.max(mag)  # normalize (Q&D)
+# don't think this normalization is necessary
+#mag *= 255.0 / numpy.max(mag)  # normalize (Q&D)
 
-scipy.misc.imsave('sobel.png', mag)
+# visualize
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+
+plt.gray() # show the filtered result in grayscale
+
+# plotting images vertically
+plt.subplot(411)
+plt.imshow(im, cmap=plt.cm.gray) # note cmap=plt.cm.gray is redundant
+plt.axis('off')
+plt.title('Original', fontsize=14)
+
+plt.subplot(412)
+plt.imshow(dx, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('Horizontal', fontsize=14)
+
+plt.subplot(413)
+plt.imshow(dy, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('Vertical', fontsize=14)
+
+plt.subplot(414)
+plt.imshow(mag, cmap=plt.cm.gray)
+plt.axis('off')
+plt.title('Sobel', fontsize=14)
+
+plt.show()
+
+# uncomment if want to save sobel image
+#scipy.misc.imsave('sobel.png', mag)
